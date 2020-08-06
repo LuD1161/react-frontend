3b3d363642855f52e82228c0568ef5cd212b0c24
Aseem Shrey <aseemshrey@gmail.com>
2020-08-06 08:00:48 +0530

    Class Component - Changed details to class component

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,50 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
+class Details extends React.Component {
+  // constructor and super needs to be done, period
+  constructor(props) {
+    super(props);
 
-const Details = (props) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+    // does shallow merge ( if confused, read about this, frontend masters : https://frontendmasters.com/courses/complete-react-v5/class-components/ )
+    this.state = {
+      loading: true,
+    };
+  }
+  // hooks don't work with class component
+  // runs only once, so use this to get the ajax data and similar kind of things
+  componentDidMount() {
+    // props are passed from parent to child and are read-only
+    // '=>' is important here because that maintains the 'context'
+    // so 'this' references the correct thing
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
+  }
+  // hard requirement for class component is `render`
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+    const { animal, breed, location, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
