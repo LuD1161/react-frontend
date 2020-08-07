e42d93ffb60d7b7296b56ce821cc7cabfa2b8e40
LuD1161 <aseemshrey@gmail.com>
2020-08-07 09:05:41 +0530

    Added theme global read

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from './ThemeContext';
 
 class Details extends React.Component {
   state = { loading: true };
@@ -35,7 +36,21 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          {/* Since we can't pass hooks to class so we use it like this */}
+          <ThemeContext.Consumer>
+            {(themeHook) => (
+              <button style={{ backgroundColor: themeHook[0] }}>
+                Adopt {name}
+              </button>
+            )}
+
+            {/* {([theme]) => (
+              <button style={{ backgroundColor: theme }}>
+                Adopt {name}
+              </button>
+            )} */}
+
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
