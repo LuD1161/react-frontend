b5a4ef7e5f1fde13ccc203187930f823caa084e3
LuD1161 <aseemshrey@gmail.com>
2020-08-07 09:16:06 +0530

    Anchor tag and DOM reload, theme value persistence

--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from '@reach/router';
 export default function Pet({ name, animal, breed, media, location, id }) {
   let hero = "http://placecorgi.com/300/300";
   if (media.length) {
@@ -6,7 +7,12 @@ export default function Pet({ name, animal, breed, media, location, id }) {
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    // <a> i.e. anchor tag was rebuilding the DOM
+    // But we don't want that
+    // Using `Link` it uses html history to render the page
+    // Thus preserving the DOM and thus saving the context color
+    // It's only if you use <a>
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -14,6 +20,6 @@ export default function Pet({ name, animal, breed, media, location, id }) {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 }
